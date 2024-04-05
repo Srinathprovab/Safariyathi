8941cde19a14d492860f71076430ab290f10452d
Srinath <Srinathprovab@gmail.com>
2024-04-05 13:00:57 +0530

    Initial Commit

---
+++ Safariyati/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Safariyati
+//
+//  Created by FCI on 05/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
